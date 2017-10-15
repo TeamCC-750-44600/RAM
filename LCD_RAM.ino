c512febff7eac2e6746ddf35a3325ad205f864a4
AwesomeFalcon <32423753+AwesomeFalcon@users.noreply.github.com>
2017-10-15 10:38:32 -0500

    Add files via upload

---
+++ LCD_RAM.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd (12, 11, 5, 4, 3, 2);
+
+void setup() {
+  
+}
+
+void loop() {
+ lcd.begin (16, 2);
+ lcd.print ("Help! Circuits");
+ lcd.setCursor (0,1);
+ lcd.print ("running slow...");
+ lcd.clear();
+}
+
+
+
